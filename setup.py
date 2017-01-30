ef8dc2b2b318e0d82781209bda12957fa19a93ff
marcindbar <marcindbar@gmail.com>
2017-01-30 10:04:34 +0100

    Add mako templates

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'setuptools',
         'Flask',
+        'Flask-Mako',
     ],
     entry_points="""
     [console_scripts]
--- src/presence_analyzer/main.py
+++ src/presence_analyzer/main.py
@@ -3,6 +3,9 @@
 Flask app initialization.
 """
 from flask import Flask
+from flask_mako import MakoTemplates
 
 
 app = Flask(__name__)  # pylint: disable=invalid-name
+mako = MakoTemplates()  # pylint: disable=invalid-name
+mako.init_app(app)
